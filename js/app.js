662c90a33dcba1f7ede96f848990bdaa8d8cc6af
graphicbeacon <jeropp00@gmail.com>
2017-03-10 12:49:42 +0000

    Implemented EventEmitter class.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,15 @@ ReactDOM.render(
     React.createElement(Forum, null),
     document.getElementById('forum')
 )
+
+var myEmitter = new EventEmitter();
+
+myEmitter.on('STARTED_THE_APP', function() {
+    console.log('started the app')
+})
+
+myEmitter.on('STARTED_THE_APP', function() {
+    console.log('started the app #2')
+})
+
+myEmitter.emit('STARTED_THE_APP');
